55c98c5132e15a413e36c95b910baafe24d6b1e3
Hannah <45151652+shed-00@users.noreply.github.com>
2021-01-16 13:12:22 -0500

    🔥 Remove several default React files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>
+          JavaScript Capstone
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
