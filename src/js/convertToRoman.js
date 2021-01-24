4563bb8d15e04d70a516bee13ef822edaf551a1c
Hannah <45151652+shed-00@users.noreply.github.com>
2021-01-24 14:55:22 -0500

    😄 js: Solve convertToRoman for '1', 2, & 3493.

--- src/js/convertToRoman.js
+++ src/js/convertToRoman.js
@@ -20,91 +20,58 @@
  * and `for` loops
  */
 const convertToRoman = (num) => {
-  if (isNaN(parseInt(num))) {
+  let parseNum = parseInt(num);
+  let mutateNum = parseNum;
+  if (isNaN(parseNum) || parseNum > 9999 || parseNum < 0) {
     return;
   }
-  let returnVal = [];
-  let trackNum = num;
-  console.log(trackNum % 1);
-  if (trackNum % 1000 < 1) {
-    for (let i = 0; i <= Math.floor(trackNum % 1000); i++) {
-      returnVal.push("M");
-      trackNum = trackNum - 1000;
+  const pusher = (floor, numMin, numMid, numMax) => {
+    if (floor > 10){
+      return;
+    }
+    else if (floor <= 3) {
+      for (let i = 0; i < floor; i++) {
+        returnArr.push(numMin);
+      }
+    }
+    else if (floor === 4) {
+      returnArr.push(numMin.concat(numMid));
+    }
+    else if (floor === 5) {
+      returnArr.push(numMid);
+    }
+    else if (floor > 5 && floor < 9) {
+      returnArr.push(numMid);
+      for (let i = 0; i < floor; i++) {
+        returnArr.push(numMin);
+      }
+    }
+    else {
+      returnArr.push(numMin.concat(numMax));
     }
   }
-  if (trackNum % 900 < 1) {
-    for (let i = 0; i <= Math.floor(trackNum % 900); i++) {
-      returnVal.push("CM");
-      trackNum = trackNum - 900;
+  const returnArr = [];
+  if (parseNum > 999) {
+    let floor = Math.floor(parseNum / 1000);
+    for (let i = 0; i < floor; i++) {
+      returnArr.push("M");
+      mutateNum = mutateNum - 1000;
     }
   }
-  if (trackNum % 500 < 1) {
-    for (let i = 0; i <= Math.floor(trackNum % 500); i++) {
-      returnVal.push("D");
-      trackNum = trackNum - 500;
-    }
+  if (mutateNum >= 100) {
+    let floor = Math.floor(mutateNum / 100);
+    pusher(floor, "C", "D", "M");
+    mutateNum = mutateNum - (floor * 100);
   }
-  if (trackNum % 400 < 1) {
-    for (let i = 0; i <= Math.floor(trackNum % 400); i++) {
-      returnVal.push("CD");
-      trackNum = trackNum - 400;
-    }
+  if (mutateNum >= 10) {
+    let floor = Math.floor(mutateNum / 10);
+    pusher(floor, "X", "L", "C");
+    mutateNum = mutateNum - (floor * 10);
   }
-  if (trackNum % 100 < 1) {
-    for (let i = 0; i <= Math.floor(trackNum % 300); i++) {
-      returnVal.push("C");
-      trackNum = trackNum - 100;
-    }
+  if (mutateNum >= 1) {
+    pusher(mutateNum, "I", "V", "X");
   }
-  if (trackNum % 90 < 1) {
-    for (let i = 0; i <= Math.floor(trackNum % 90); i++) {
-      returnVal.push("XC");
-      trackNum = trackNum - 90;
-    }
-  }
-  if (trackNum % 50 < 1) {
-    for (let i = 0; i <= Math.floor(trackNum % 50); i++) {
-      returnVal.push("L");
-      trackNum = trackNum - 50;
-    }
-  }
-  if (trackNum % 40 < 1) {
-    for (let i = 0; i <= Math.floor(trackNum % 40); i++) {
-      returnVal.push("XL");
-      trackNum = trackNum - 40;
-    }
-  }
-  if (trackNum % 10 < 1) {
-    for (let i = 0; i <= Math.floor(trackNum % 10); i++) {
-      returnVal.push("X");
-      trackNum = trackNum - 10;
-    }
-  }
-  if (trackNum % 9 < 1) {
-    for (let i = 0; i <= Math.floor(trackNum % 9); i++) {
-      returnVal.push("IX");
-      trackNum = trackNum - 9;
-    }
-  }
-  if (trackNum % 5 < 1) {
-    for (let i = 0; i <= Math.floor(trackNum % 5); i++) {
-      returnVal.push("V");
-      trackNum = trackNum - 5;
-    }
-  }
-  if (trackNum % 4 < 1) {
-    for (let i = 0; i <= Math.floor(trackNum % 4); i++) {
-      returnVal.push("IV");
-      trackNum = trackNum - 4;
-    }
-  }
-  if (trackNum % 1 < 1) {
-    for (let i = 0; i <= Math.floor(trackNum % 1); i++) {
-      returnVal.push("I");
-      trackNum = trackNum - 1;
-    }
-  }
-  return returnVal.join("");
+  return returnArr.join("");
 }
 
 module.exports = convertToRoman;
@@ -112,4 +79,4 @@ module.exports = convertToRoman;
 console.log(convertToRoman("quack"));
 console.log(convertToRoman(2));
 console.log(convertToRoman("1"));
-console.log(convertToRoman(3000));
+console.log(convertToRoman(3493));
