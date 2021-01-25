55650d186f575fb89743f7f3f8d55d3c02f67de2
Hannah <45151652+shed-00@users.noreply.github.com>
2021-01-25 15:10:05 -0500

    ✅ rot13 cipher: Finish script using `charCodeAt`, `String.fromCharCode`, and `map`

---
+++ src/js/rot13.js
@@ -0,0 +1,31 @@
+/**
+ *
+ * @param {String} str
+ * ROT13 shift cipher:
+ * shift the letters in the string by
+ * 13; e.g., A ==> N
+ */
+const rot13 = (str) => {
+  const charArr = [...str];
+  const minChar = "A".charCodeAt();
+  const maxChar = "Z".charCodeAt();
+  return charArr.map((char) => {
+    let charToCheck = char.charCodeAt();
+    return (charToCheck < minChar || charToCheck > maxChar)
+      ? String.fromCharCode(charToCheck)
+      : ((charToCheck - 13) < minChar)
+        ? String.fromCharCode(maxChar - (13 - (charToCheck - minChar + 1)))
+        : String.fromCharCode(charToCheck - 13);
+  }).join("");
+}
+
+module.exports = rot13;
+
+// FREE CODE CAMP
+console.log(rot13("SERR PBQR PNZC"))
+
+// FREE PIZZA!
+console.log(rot13("SERR CVMMN!"))
+
+// FREE LOVE?
+console.log(rot13("SERR YBIR?"))
