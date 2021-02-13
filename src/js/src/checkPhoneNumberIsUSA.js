2f9d9fe8584631b398a6bd44dd76bd09d27ca7ce
hdevilbiss <45151652+hdevilbiss@users.noreply.github.com>
2021-02-13 15:28:56 -0500

    ☎️ phoneCheck: trim, & return false for nums < 10

--- src/js/src/checkPhoneNumberIsUSA.js
+++ src/js/src/checkPhoneNumberIsUSA.js
@@ -1,10 +1,13 @@
 /**
  *
  * @param {String} str
- * Return whether a given string is a valid U.S. phone number
+ * Return whether a given string is a valid U.S. phone number: T/F
  */
 const checkPhoneNumberIsUSA = (str) => {
-  return str;
+  let trimStr = str.trim();
+  if (trimStr.length < 10) return false;
+  // let parenthesesRegex = "";
+  return true;
 }
 
 module.exports = checkPhoneNumberIsUSA;
