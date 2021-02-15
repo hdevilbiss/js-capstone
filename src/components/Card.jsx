23f6b3a99631d5e29470b982a881d28b4fd678fc
hdevilbiss <45151652+hdevilbiss@users.noreply.github.com>
2021-02-15 18:13:33 -0500

    🎨 Add data.js to Card.jsx using props

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,43 @@
 import slugify from "../js/lib/slugify.js";
 
-const Card = ({ title, promptLink }) => {
+const Card = ({ name, heading, promptLink, inputLabel, inputHandler, inputValue, inputFeedback, submitHandler, submitButtonText, clearHandler, clearButtonText, outputArray, bgArtistLink, bgArtistName, bgPhotoLink, bgPhotoVendor }) => {
   return (
-    <div className={`card ${slugify(title) ?? ""}`}>
+    <div className={`card ${slugify(name) ?? ""}`}>
       <header>
         <h2>
-          {title}
+          {heading}
         </h2>
-        <a href={`${promptLink ?? ""}`}></a>
+        <a href={promptLink}>
+          Prompt
+        </a>
       </header>
+      <section>
+        <div>
+          <label htmlFor={`${slugify(name)}-input`}>
+          {inputLabel}
+          </label>
+          <input type="text" id={`${slugify(name)}`} onChange={inputHandler} value={inputValue} />
+          <span className="feedback">
+          {inputFeedback}
+          </span>
+        </div>
+        <div>
+          <button className="button-input" onClick={submitHandler}>
+            {submitButtonText}
+          </button>
+          <button className="button-clear" onClick={clearHandler}>
+            {clearButtonText}
+          </button>
+        </div>
+        <div className="output">
+          {outputArray.map((str, idx) => (
+            <p key={idx}>{str}</p>
+          ))}
+        </div>
+        <div className="photo-credit">
+          Photo by <a href={bgArtistLink}>{bgArtistName}</a> on <a href={bgPhotoLink}>{bgPhotoVendor}</a>
+        </div>
+      </section>
     </div>
   );
 }
--- src/data.js
+++ src/data.js
@@ -4,20 +4,17 @@ function MakeCards() {
     {
       name: "Palindrome",
       id: uuidv4(),
-      jsPath: "./js/palindrome.js",
-      scssPath: "./style/_palindrome.scss",
-      validation: [
-        {
-          regex: "^\w{2,20}$",
-          message: "Must be between 2 and 20 characters"
-        }
-      ],
       heading: "Palindrome Tester",
       promptLink: "https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/javascript-algorithms-and-data-structures-projects/palindrome-checker",
-      sourceLink: "https://github.com/hdevilbiss/js-capstone/blob/main/src/js/palindrome.js",
       inputLabel: "Input a word",
       submitButtonText: "Test the word",
       clearButtonText: "Clear output",
+      validation: [
+        {
+          regex: /^\w{2,20}$/,
+          message: "Must be between 2 and 20 characters"
+        }
+      ],
       bgArtistLink: "https://unsplash.com/@andyjh07?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText",
       bgArtistName: "Andy Holmes",
       bgPhotoLink: "https://unsplash.com/s/photos/night-sky?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText",
@@ -30,7 +27,6 @@ function MakeCards() {
       scssPath: "",
       heading: "Palindrome Tester",
       promptLink: "",
-      sourceLink: "",
       inputLabel: "",
       submitButtonText: "",
       clearButtonText: "",
