feaf20317063a6940347ec96a166451a0eb685aa
hdevilbiss <45151652+hdevilbiss@users.noreply.github.com>
2021-02-15 20:40:25 -0500

    ✨ components: Use Card. Convert feedback to array.

--- src/components/Roman.jsx
+++ src/components/Roman.jsx
@@ -1,36 +1,37 @@
 import { useState } from "react";
 import convertToRoman from "../js/lib/convertToRoman";
 import "../style/_roman.scss";
+import Card from "../components/Card";
 
-const Roman = () => {
+const Roman = ({ object }) => {
   /**
    * State
    */
-  const [romanInput, setromanInput] = useState("");
-  const [romanFeedback, setromanFeedback] = useState("");
-  const [romanOutput, setromanOutput] = useState([]);
+  const [romanInput, setRomanInput] = useState("");
+  const [romanFeedback, setRomanFeedback] = useState([]);
+  const [romanOutput, setRomanOutput] = useState([]);
 
   /**
    * Event Handlers
    */
   const romanInputHandler = (event) => {
-    setromanInput(event.target.value);
+    setRomanInput(event.target.value);
   };
   const submitRomanHandler = () => {
     if (romanInput.length < 1 || romanInput.length > 4) {
-      setromanFeedback("Should be between 1 and 4 characters in length");
+      setRomanFeedback("Should be between 1 and 4 characters in length");
     }
     else {
-      setromanOutput([...romanOutput, convertToRoman(romanInput)]);
-      setromanFeedback("");
-      setromanInput("");
+      setRomanOutput([...romanOutput, convertToRoman(romanInput)]);
+      setRomanFeedback("");
+      setRomanInput("");
     }
     document.getElementById("roman-input").focus();
   }
   const clearRomanHandler = () => {
-    setromanInput("");
-    setromanFeedback("");
-    setromanOutput([]);
+    setRomanInput("");
+    setRomanFeedback("");
+    setRomanOutput([]);
     document.getElementById("roman-input").focus();
   };
 
@@ -38,37 +39,16 @@ const Roman = () => {
    * Return object
    */
   return (
-    <div className="roman card">
-      <header>
-        <h2>
-          Convert a number less than 9999 to a Roman Numeral
-        </h2>
-        <a href="https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/javascript-algorithms-and-data-structures-projects/roman-numeral-converter">
-          Exercise Prompt
-        </a>
-      </header>
-      <div>
-        <label htmlFor="roman-input">
-          Enter a number from 1 to 9999
-        </label>
-        <input type="number" id="roman-input" onChange={romanInputHandler} value={romanInput} />
-        <span className="feedback">{romanFeedback}</span>
-      </div>
-      <button className="button-input" onClick={submitRomanHandler}>
-        Convert the number
-      </button>
-      <button className="button-clear" onClick={clearRomanHandler}>
-        Clear the output
-      </button>
-      <div className="output">
-        {romanOutput.map((str, idx) => (
-          <p key={idx}>{str}</p>
-        ))}
-      </div>
-      <div className="photo-credit">
-        Photo by <a href="https://unsplash.com/@lilianovich?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Lilian Dibbern</a> on <a href="https://unsplash.com/s/photos/roman?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a>
-      </div>
-    </div>
+    <Card
+      object={object}
+      inputHandler={romanInputHandler}
+      inputValue={romanInput}
+      inputFeedback={romanFeedback}
+      submitHandler={submitRomanHandler}
+      clearHandler={clearRomanHandler}
+      outputArray={romanOutput}
+    >
+    </Card>
   );
 }
 
--- src/components/Rot.jsx
+++ src/components/Rot.jsx
@@ -1,36 +1,37 @@
 import { useState } from "react";
 import rot13 from "../js/lib/rot13";
 import "../style/_rot.scss";
+import Card from "./Card";
 
-const Rot = () => {
+const Rot = ({ object }) => {
   /**
    * State
    */
-  const [rotInput, setrotInput] = useState("");
-  const [rotFeedback, setrotFeedback] = useState("");
-  const [rotOutput, setrotOutput] = useState([]);
+  const [rotInput, setRotInput] = useState("");
+  const [rotFeedback, setRotFeedback] = useState([]);
+  const [rotOutput, setRotOutput] = useState([]);
 
   /**
    * Event Handlers
    */
   const rotInputHandler = (event) => {
-    setrotInput(event.target.value);
+    setRotInput(event.target.value);
   };
   const submitRotHandler = () => {
     if (rotInput.length < 1 || rotInput.length > 200) {
-      setrotFeedback("Should be between 1 and 200 characters in length");
+      setRotFeedback("Should be between 1 and 200 characters in length");
     }
     else {
-      setrotOutput([...rotOutput, rot13(rotInput)]);
-      setrotFeedback("");
-      setrotInput("");
+      setRotOutput([...rotOutput, rot13(rotInput)]);
+      setRotFeedback([]);
+      setRotInput("");
     }
     document.getElementById("rot-input").focus();
   }
   const clearRotHandler = () => {
-    setrotInput("");
-    setrotFeedback("");
-    setrotOutput([]);
+    setRotInput("");
+    setRotFeedback([]);
+    setRotOutput([]);
     document.getElementById("rot-input").focus();
   };
 
@@ -38,37 +39,16 @@ const Rot = () => {
    * Return object
    */
   return (
-    <div className="rot card">
-      <header>
-        <h2>
-          Decode a rot13 ciphered string
-        </h2>
-        <a href="https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/javascript-algorithms-and-data-structures-projects/caesars-cipher">
-          Exercise Prompt
-        </a>
-      </header>
-      <div>
-        <label htmlFor="rot-input">
-          Enter a rot13-encoded string to decode it. Only UPPERCASE characters are applicable.
-        </label>
-        <input type="text" id="rot-input" onChange={rotInputHandler} value={rotInput} />
-        <span className="feedback">{rotFeedback}</span>
-      </div>
-      <button className="button-input" onClick={submitRotHandler}>
-        Decode the STRING
-      </button>
-      <button className="button-clear" onClick={clearRotHandler}>
-        Clear the output
-      </button>
-      <div className="output">
-        {rotOutput.map((str, idx) => (
-          <p key={idx}>{str}</p>
-        ))}
-      </div>
-      <div className="photo-credit">
-        Photo by <a href="https://unsplash.com/@markusspiske?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Markus Spiske</a> on <a href="https://unsplash.com/s/photos/binary?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a>
-      </div>
-    </div>
+    <Card
+      object={object}
+      inputHandler={rotInputHandler}
+      inputValue={rotInput}
+      inputFeedback={rotFeedback}
+      submitHandler={submitRotHandler}
+      clearHandler={clearRotHandler}
+      outputArray={rotOutput}
+    >
+    </Card>
   );
 }
 
