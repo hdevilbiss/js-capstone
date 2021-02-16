fbc3a7348265d62656be7acad5a9a5087b8990ca
Hannah <45151652+shed-00@users.noreply.github.com>
2021-02-16 14:06:24 -0500

    🛠️ Begin checkCashRegister & write unit tests

--- src/js/src/checkCashRegister.js
+++ src/js/src/checkCashRegister.js
@@ -1,12 +1,22 @@
 const checkCashRegister = ([price, cash, cid]) => {
-  if (cash - price < 0) return {status: "INSUFFICIENT FUNDS", change: []};
+  let change = cash - price;
+  console.log(`change: ${change}`);
+  if (change < 0) {
+    return {status: "INSUFFICIENT FUNDS", change: []};
+  }
   const cashInDrawer = cid.map(cashArr => {
     let amt = cashArr[1];
-    let qty = Math.floor(amt);
-    let val = amt % 1;
-    return qty * val;
+    return Math.floor(amt) * (amt % 1);
   }).reduce((sum, curr) => sum + curr);
-  if (cashInDrawer < (cash - price)) return {status: "INSUFFICIENT FUNDS", change: []};
+  if (cashInDrawer < change) {
+    return {status: "INSUFFICIENT FUNDS", change: []};
+  }
+  else if (cashInDrawer === change) {
+    return {status: "CLOSED", change: cid};
+  }
+  else {
+    return {status: "OPEN", change: []};
+  }
 }
 
 module.exports = checkCashRegister;
---
+++ test/checkCashRegister.spec.js
@@ -0,0 +1,35 @@
+const assert = require("assert");
+const checkCashRegister = require("../src/js/src/checkCashRegister");
+
+describe("Test suite for checkCashRegister", () => {
+  const testArr = [];
+
+  testArr.push(
+    [
+      [19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]], {status: "OPEN", change: [["QUARTER", 0.5]]}
+    ],
+    [
+      [3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]], {status: "OPEN", change: [["TWENTY", 60], ["TEN", 20], ["FIVE", 15], ["ONE", 1], ["QUARTER", 0.5], ["DIME", 0.2], ["PENNY", 0.04]]}
+    ],
+    [
+      [19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]], {status: "INSUFFICIENT_FUNDS", change: []}
+    ],
+    [
+      [19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]], {status: "INSUFFICIENT_FUNDS", change: []}
+    ],
+    [
+      [19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]], {status: "CLOSED", change: [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]}
+    ]
+  );
+
+  it(`should return an object given ${testArr[0][0]}`, () => {
+    assert.strictEqual(typeof checkCashRegister(testArr[0][0]), "object");
+  });
+
+  // Loop through all tests
+  testArr.forEach(([input, expectedResult]) => {
+    it(`should return ${JSON.stringify(expectedResult)} given ${input}`, () => {
+      assert.deepStrictEqual(checkCashRegister(input), expectedResult);
+    });
+  });
+});
