8be1b8f84ac72d1ad178e5f3f7fbae9de55c0854
Hannah <45151652+shed-00@users.noreply.github.com>
2021-02-18 12:13:39 -0500

    💩 Cash Register: Get cashInDrawer with map & find

--- src/js/src/checkCashRegister.js
+++ src/js/src/checkCashRegister.js
@@ -1,22 +1,31 @@
+/**
+ *
+ * @param {Array}
+ * Given an array containing the price of a purchase,
+ * the amount of cash given by a customer,
+ * and the cash-in-drawer (cid) array,
+ * return the customer's change
+ * as an object with a status and change key.
+ */
 const checkCashRegister = ([price, cash, cid]) => {
+  const refArr = [
+    ["PENNY", 0.01],
+    ["NICKEL", 0.05],
+    ["DIME", 0.1],
+    ["QUARTER", 0.25],
+    ["ONE", 1],
+    ["FIVE", 5],
+    ["TEN", 10],
+    ["TWENTY", 20],
+    ["ONE HUNDRED", 100]
+  ];
+  const cashInDrawer = cid.map(([currency, amount]) => {
+    const unitOfCurrency = refArr.find(arr => arr[0] === currency);
+    return amount / unitOfCurrency[1];
+  }).reduce((sum, curr) => sum + curr);
+  console.log(`cashInDrawer: ${cashInDrawer}`);
   let change = cash - price;
   console.log(`change: ${change}`);
-  if (change < 0) {
-    return {status: "INSUFFICIENT FUNDS", change: []};
-  }
-  const cashInDrawer = cid.map(cashArr => {
-    let amt = cashArr[1];
-    return Math.floor(amt) * (amt % 1);
-  }).reduce((sum, curr) => sum + curr);
-  if (cashInDrawer < change) {
-    return {status: "INSUFFICIENT FUNDS", change: []};
-  }
-  else if (cashInDrawer === change) {
-    return {status: "CLOSED", change: cid};
-  }
-  else {
-    return {status: "OPEN", change: []};
-  }
 }
 
 module.exports = checkCashRegister;
