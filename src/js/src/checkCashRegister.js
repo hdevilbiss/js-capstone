61f32895dc80483a2052e6849eeb5af0a6f3d0ca
hdevilbiss <45151652+hdevilbiss@users.noreply.github.com>
2021-02-21 12:43:31 -0500

    🚧 CashRegister: Commit before starting over

--- src/js/src/checkCashRegister.js
+++ src/js/src/checkCashRegister.js
@@ -33,9 +33,9 @@ const checkCashRegister = (price = 0, cash = 0, cid = []) => {
     return sum + currArr[1];
   }, 0) * 100) / 100;
 
-  // console.log(`cid: ${JSON.stringify(cid)}`);
-  // console.log(`cashInDrawer: ${cashInDrawer}`);
-  // console.log(`cashDue: ${changeAmount}`);
+  console.log(`cid: ${JSON.stringify(cid)}`);
+  console.log(`cashInDrawer: ${cashInDrawer}`);
+  console.log(`changeAmount: ${changeAmount}`);
 
   /** Not enough cash in register or not enough cash given by customer */
   if (cashInDrawer < changeAmount || changeAmount < 0) {
@@ -55,14 +55,15 @@ const checkCashRegister = (price = 0, cash = 0, cid = []) => {
       let neededQty = Math.floor(remainingBalance / value);
       let actualQty = Math.floor(cid[i][1] / value);
 
-      // console.log(`neededQty: ${neededQty}`);
-      // console.log(`actualQty: ${actualQty}`);
+      console.log(`neededQty of ${value}: ${neededQty}`);
+      console.log(`actualQty of ${value}: ${actualQty}`);
 
       if (neededQty > 0 && actualQty >= neededQty) {
         returnChange.change = [...returnChange.change, [currency, neededQty * value]];
         remainingBalance %= value;
       }
     }
+    console.log(`remainingBalance: ${remainingBalance}`);
     if (remainingBalance !== 0) {
       returnChange.status = "INSUFFICIENT_FUNDS";
       returnChange.change = [];
@@ -77,3 +78,5 @@ module.exports = checkCashRegister;
 console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
 
 console.log(checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]));
+
+console.log(checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
