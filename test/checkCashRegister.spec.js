a4dbc3099d7beaec70466fc7611f18c1c8cef44b
Hannah <45151652+shed-00@users.noreply.github.com>
2021-02-24 12:56:46 -0500

    💥 Copy CashRegister tests from freeCodeCamp

--- test/checkCashRegister.spec.js
+++ test/checkCashRegister.spec.js
@@ -6,7 +6,7 @@ describe("Test suite for checkCashRegister", () => {
 
   testArr.push(
     [
-      [19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]], {status: "OPEN", change: [["QUARTER", 0.5]]}
+      [19.5, 20, [['PENNY', 1.01],['NICKEL', 2.05],['DIME', 3.1],['QUARTER', 4.25],['ONE', 90],['FIVE', 55],['TEN', 20],['TWENTY', 60],['ONE HUNDRED', 100]]],{status: 'OPEN', change: [['QUARTER', 0.5]]}
     ],
     [
       [3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]], {status: "OPEN", change: [["TWENTY", 60], ["TEN", 20], ["FIVE", 15], ["ONE", 1], ["QUARTER", 0.5], ["DIME", 0.2], ["PENNY", 0.04]]}
@@ -19,7 +19,7 @@ describe("Test suite for checkCashRegister", () => {
     ],
     [
       [19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]], {status: "CLOSED", change: [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]}
-    ]
+    ],
   );
 
   it(`should return an object given ${testArr[0][0]}`, () => {
@@ -29,7 +29,43 @@ describe("Test suite for checkCashRegister", () => {
   // Loop through all tests
   testArr.forEach(([[price, cash, cid], expectedResult]) => {
     it(`should return ${JSON.stringify(expectedResult)} given ${price}, ${cash}, ${JSON.stringify(cid)}`, () => {
-      assert.deepStrictEqual(checkCashRegister(price, cash, cid), expectedResult);
+      assert.deepStrictEqual(
+        checkCashRegister(price, cash, cid), expectedResult
+      );
     });
+  });
+
+  it(`should not return half a $5 bill when the change is 2 quarters and the details are copied directly from freeCodeCamp`, () => {
+    assert.deepStrictEqual(
+      checkCashRegister(19.5, 20, [
+        ['PENNY', 1.01],
+        ['NICKEL', 2.05],
+        ['DIME', 3.1],
+        ['QUARTER', 4.25],
+        ['ONE', 90],
+        ['FIVE', 55],
+        ['TEN', 20],
+        ['TWENTY', 60],
+        ['ONE HUNDRED', 100]
+      ]),
+      { status: 'OPEN', change: [['QUARTER', 0.5]] }
+    );
+  });
+
+  it(`should also not return half a $5 bill when the change is 2 quarters`, () => {
+    assert.deepStrictEqual(
+      checkCashRegister(19.5, 20, [
+        ["PENNY", 1.01],
+        ["NICKEL", 2.05],
+        ["DIME", 3.1],
+        ["QUARTER", 4.25],
+        ["ONE", 90],
+        ["FIVE", 55],
+        ["TEN", 20],
+        ["TWENTY", 60],
+        ["ONE HUNDRED", 100]
+      ]),
+      {status: "OPEN", change: [["QUARTER", 0.5]]}
+    );
   });
 });
