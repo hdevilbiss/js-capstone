e8512b773bf1124b83e68c1736753a53e01b701b
Hannah <45151652+shed-00@users.noreply.github.com>
2021-02-25 13:04:19 -0500

    🚧 CashRegister: Add state, select input & buttons

--- src/components/CashRegister.jsx
+++ src/components/CashRegister.jsx
@@ -1,14 +1,50 @@
+import { useState } from "react";
 import checkCashRegister from "../js/lib/checkCashRegister";
+import PhotoCredit from "./partials/PhotoCredit";
+import products from "../products";
+import MakeProducts from "../products";
+import GetCurrencies from "../currency";
 
 const CashRegister = ({ object }) => {
+  /**
+   * State
+   */
+  const [products, setProducts] = useState(MakeProducts());
+  const [currencies, setCurrencies] = useState(GetCurrencies());
+
+
   return (
-    <div>
+    <div className="cash-register card">
       <section>
         <header>
           <h2>
             { object.heading }
           </h2>
         </header>
+        <label htmlFor="products">Products</label>
+        <select name="selectedProduct" id="products">
+          {products.sort((a, b) => a.name > b.name).map(product => {
+            return (
+              <option value={product.name} key={product.id}>
+                {product.name} {product.emoji}
+              </option>
+            );
+          })}
+        </select>
+        <button>
+          Add to cart
+        </button>
+        <div className="currencies">
+          {currencies.map(({currency, id, value, type, color}) => {
+            if (type === "coin") {
+              return (
+                <button>
+                  {currency}: {value}
+                </button>
+              );
+            }
+          })}
+        </div>
       </section>
     </div>
   );
